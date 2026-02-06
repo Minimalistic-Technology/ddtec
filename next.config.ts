714255721a15ab6b903eb5f2237142edca1a8eb6
Saadali___114 <saadali92486@gmail.com>
2026-02-06 20:47:52 +0530

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*', // Proxy to Backend
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
       },
-    ]
-  }
+    ];
+  },
 };
 
 export default nextConfig;
